80f31d8e86842ae6088661356c47300311638eaa
AverageSecurityGuy <stephen@averagesecurityguy.info>
2016-02-22 17:32:04 -0600

    Rebuilt script to only count a subdomain once when multiple name servers allow zone transfers.

--- stats.py
+++ stats.py
@@ -4,28 +4,28 @@ import os
 
 domains = []
 nservers = []
-subs = {}
-
-
-def process_line(line):
-    if 'DOMAIN: ' in line:
-        domains.append(line.split(' ')[1])
-    if 'NS: ' in line:
-        nservers.append(line.split(' ')[1])
-
-    if ' IN ' in line:
-        sub = line.split(' ')[0]
-
-        if sub in subs:
-            subs[sub] += 1
-        else:
-            subs[sub] = 1
+subdomains = {}
 
 
 def process_file(fn):
+    subs = []
     with open(fn) as f:
         for line in f:
-            process_line(line.rstrip('\r\n'))
+            if 'DOMAIN: ' in line:
+                domains.append(line.split(' ')[1])
+            if 'NS: ' in line:
+               nservers.append(line.split(' ')[1])
+
+            if ' IN ' in line:
+                subs.append(line.split(' ')[0])
+
+    # Make sure I only count a subdomain once when multiple DNS servers allow
+    # zone transfers.
+    for s in set(subs):
+        if s in subdomains:
+            subdomains[s] += 1
+        else:
+            subdomains[s] = 1
 
 
 for fn in os.listdir('.'):
@@ -48,16 +48,16 @@ nstats.extend(sorted(nservers))
 
 sstats = ['Sub Domains By Count']
 sstats.append('--------------------')
-sstats.append('Count: {0}'.format(len(subs)))
+sstats.append('Count: {0}'.format(len(subdomains)))
 sstats.append('')
-sorted_subs = sorted(subs.items(), key=lambda x: x[1], reverse=True)
+sorted_subs = sorted(subdomains.items(), key=lambda x: x[1], reverse=True)
 sstats.extend(['{0}: {1}'.format(s[0], s[1]) for s in sorted_subs])
 
-with open('domains.md', 'w') as f:          
+with open('domains.txt', 'w') as f:          
     f.write('\n'.join(dstats))
 
-with open('nameservers.md', 'w') as f:          
+with open('nameservers.txt', 'w') as f:          
     f.write('\n'.join(nstats))
 
-with open('subdomains.md', 'w') as f:          
+with open('subdomains.txt', 'w') as f:          
     f.write('\n'.join(sstats))
